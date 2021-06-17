3e489b69e58064e8cfd70facf885a8240f02594a
Marina Wenzel <78405556+MariWe@users.noreply.github.com>
2021-06-17 14:29:08 +0200

    Update script.js
    anpassen der setAttribute

--- script.js
+++ script.js
@@ -24,10 +24,8 @@ function renderPlaces(places) {
 
         let model = document.createElement('a-entity');
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model.setAttribute('obj-model', './assets/Fisch/Skulptur.obj');
-        model.setAttribute('mtl-model', './assets/Fisch/Texturemap.mtl');
+        model.setAttribute('obj-model', 'obj: url(/ArSkulpturen/assets/Fisch/Skulptur.obj; mtl: url(/ArSkulpturen/assets/Fisch/Texturemap.mtl;');
         model.setAttribute('rotation', '0 0 0');
-        model.setAttribute('animation-mixer', '');
         model.setAttribute('scale', '1 1 1');
 
         model.addEventListener('loaded', () => {
