00dd8ba700d7b32b0378fc47531ca25ea5c36be4
Marina Wenzel <78405556+MariWe@users.noreply.github.com>
2021-06-17 14:13:17 +0200

    Create script.js
    Create object

---
+++ script.js
@@ -0,0 +1,39 @@
+window.onload = () => {
+     let places = staticLoadPlaces();
+     renderPlaces(places);
+};
+
+function staticLoadPlaces() {
+    return [
+        {
+            name: 'Fisch',
+            location: {
+                lat: 50.821981,
+                lng: 12.939613,
+            }
+        },
+    ];
+}
+
+function renderPlaces(places) {
+    let scene = document.querySelector('a-scene');
+
+    places.forEach((place) => {
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
+
+        let model = document.createElement('a-entity');
+        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        model.setAttribute('obj-model', './assets/Fisch/Skulptur.obj');
+        model.setAttribute('mtl-model', './assets/Fisch/Texturemap.mtl');
+        model.setAttribute('rotation', '0 0 0');
+        model.setAttribute('animation-mixer', '');
+        model.setAttribute('scale', '1 1 1');
+
+        model.addEventListener('loaded', () => {
+            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+        });
+
+        scene.appendChild(model);
+    });
+}
