2020e30edc8b8a2e1c299d74226ca27dcc3b0379
Marina Wenzel <78405556+MariWe@users.noreply.github.com>
2021-07-14 08:55:55 +0200

    Add files via upload

---
+++ scriptNew.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', () => {
+    const camera =  document.querySelector('[gps-camera]');
+    const pony = document.getElementById(pony);
+    let check;
+
+    pony.addEventListener('ponyFound', () => {
+        let cameraPosition = camera.object3D.position;
+        let ponyPosition = pony.object3D.position;
+        let distance = cameraPosition.distanceTo(ponyPosition)
+
+        check = setInterval(() => {
+            cameraPosition = camera.object3D.position;
+            ponyPosition = pony.object3D.position;
+            distance = cameraPosition.distanceTo(ponyPosition)
+
+            console.log(distance);
+        }, 100);
+    });
+
+    pony.addEventListener('ponyLost', () => {
+        clearInterval(check);
+    })
+})
