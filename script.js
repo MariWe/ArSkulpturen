d4805d240e46228626d2e7c7962a04f48730a0f1
Marina Wenzel <78405556+MariWe@users.noreply.github.com>
2021-07-14 14:07:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,16 @@ window.onload = () => {
     const po = document.querySelector('#pony');
     const ca = document.querySelector('#camera');
 
-    console.log('Pfeil');
-    console.log(pf.getAttribute('position'));
-  
-    console.log("Kamera");
-    console.log(ca.getAttribute('position'));
+    function pfPosition(){
+        let oldPosition = ca.getAttribute('position');
+        let newPosition = {x: oldPosition.x, y: oldPosition.y, z: -4};
 
-    let oldPosition = ca.getAttribute('position');
-    let newPosition = {x: oldPosition.x, y: oldPosition.y, z: -4};
+        pf.setAttribute('position', newPosition);
+        console.log(pf.getAttribute('position'));
+        }
 
-    pf.setAttribute('position', newPosition);
-    console.log(pf.getAttribute('position'));
+    setInterval(function() { 
+    pfPosition(); 
+}, 500);
+
 }
