3d97c54f572ba2a1b90dc21920d2a8e44ff1f15b
Marina Wenzel <78405556+MariWe@users.noreply.github.com>
2021-07-15 13:14:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 window.onload = () => {
 
-function PointingTwo(){
+function Pointing(){
     var pfeil = document.querySelector('#pfeil');
     var pony = document.querySelector('#pony');
     var position = pony.object3D.position;
 
     pfeil.object3D.lookAt(new THREE.Vector3(position.x, position.y, position.z));
 }  
-
 setInterval(function() { 
-    PointingTwo(); 
+    Pointing(); 
 }, 100);
 }
